842b7ce65a5a6186cdea883e3dec3ecdc85aa9f2
AverageAnarchist32 <127637565+AverageAnarchist32@users.noreply.github.com>
2023-08-20 12:08:20 +1000

    Update server.js

--- API/server.js
+++ API/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // Import bodyParser middleware
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json()); // Use bodyParser middleware to parse JSON requests
 
 // Function to map the analog value to the voltage range
 function floatMap(x, in_min, in_max, out_min, out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-app.get('/api/voltage', (req, res) => {
-  const analogValue = Math.floor(Math.random() * 4096); // Simulating analog value (0 to 4095)
+// Route to receive data from ESP32
+app.post('/api/voltage', (req, res) => {
+  const { analogValue } = req.body; // Extract analogValue from request body
   const voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
   res.json({ analogValue, voltage });
 });
